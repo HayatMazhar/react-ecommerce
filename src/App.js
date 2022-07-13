4cdc9090079210e502af1ea15a2920fbb275731b
Mazhar Hayat <mhayat@farahexperiences.com>
2022-07-13 23:38:53 +0400

    rename folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import axios from 'axios';
-import Header from './components/_shared/layout/Header.js';
-import Banner from './components/_shared/layout/Banner.js';
-import Footer from './components/_shared/layout/Footer.js';
-import Home from './components/Core/Home.js';
+import Header from './components/shared/layout/Header.js';
+import Banner from './components/shared/layout/Banner.js';
+import Footer from './components/shared/layout/Footer.js';
+import Home from './components/core/Home.js';
 
 class App extends React.Component {
 
